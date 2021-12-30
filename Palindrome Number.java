a0bca6c750e706b64651b3cec53a30def4316bf4
Haneen El-Sawy <haneensalah2105@gmail.com>
2021-12-30 23:34:11 +0200

    Add files via upload

---
+++ Palindrome Number.java
@@ -0,0 +1,25 @@
+public class Pallimdrome {
+
+    
+    public static void main(String[] args) {
+        int x= 1221;
+        boolean flag=true;
+        String []str= new String[10];
+        str=Integer.toString(x).split("(?!^)");
+        int r=0;
+        int l=str.length-1;
+  
+        for(int i=0; i <str.length; i++){
+            if(str[r].equals(str[l])){
+                
+                flag=true;
+                r++;
+                l--;
+            }
+            else{
+                flag=false;
+            }
+    }
+        System.out.println(flag);
+    }
+}
